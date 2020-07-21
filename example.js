c0a7288da5e98d8b0953558de1cd53da439df16c
Antonio Martin <tonitim13@gmail.com>
2020-07-21 10:31:24 -0300

    Creando un ejemplo mas practico

--- example.js
+++ example.js
@@ -1,19 +1,49 @@
 const Crawly = require('./lib/crawler');
 
 const crawly = new Crawly({
-  parse: function (err, res, done) {
+  delay: 10000,
+  parser: function (err, res, done) {
+    const self = this;
     if (err) {
       console.log(err);
       done();
     } else {
 
-      const self = this;
-      const { document } = res;
+      const { document, params } = res;
+      switch (params.page) {
+        case 'profile': {
+          const tabs = document.querySelectorAll('a.link-gray.no-underline.no-wrap');
+          if (tabs[1]) {
+            self.addQueue(`https://github.com${tabs[1].href}`, {
+              page: 'following',
+              deep: params.deep
+            });
+          }
+          break;
+        }
+        case 'following': {
+          const followingList = document.querySelectorAll('.v-align-top.pr-3 > a > span.link-gray.pl-1') || [];
+          const username = document.querySelector('.p-nickname').textContent;
+          const users = [];
+          followingList.forEach((item) => {
+            const name = item.textContent;
+            if (name) {
 
-      console.log(document.querySelector('title').innerHTML);
-      (document.querySelectorAll('.pinned-item-list-item-content a') || []).forEach(function (item) {
-        self.addQueue(`https://github.com${item.href}`);
-      });
+              users.push(name);
+
+              if (params.deep < 2) {
+                self.addQueue(`https://github.com/${name}`, {
+                  page: 'profile',
+                  deep: params.deep + 1
+                });
+              }
+            }
+          });
+
+          console.log(`${username} is following these users: ${users.join(', ')}\n`);
+          break;
+        }
+      }
 
       done();
     }
@@ -22,4 +52,7 @@ const crawly = new Crawly({
 
 crawly.run('https://github.com/antoniowd', () => {
   console.log('ok');
+}, {
+  page: 'profile',
+  deep: 0
 });
